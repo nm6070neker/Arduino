d1fe4b7fd8516cfd0e8347476d7cd422b08108d8
Maher <nmaher121110@gmail.com>
2026-02-17 14:17:38 -0500

    Added LED & lights flashing oppisite

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -30,12 +30,16 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(3000);                      // wait for a second
+  digitalWrite(9, LOW);   // turn the LED off by making the voltage LOW 
+      delay(100);                      // wait for a second
+   // wait for a second
+   digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)
   digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+  delay(100);                      // wait for a second
 }
